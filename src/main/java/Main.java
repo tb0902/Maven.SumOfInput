16acda8d04f4fe27e0b1a90f07b6b1fa87fd1b1c
tb0902 <tbruton417@gmail.com>
2024-03-01 09:00:19 -0500

    final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,18 +17,22 @@ public class Main {
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+
+        // This method reads the number provided using keyboard
+
+        int total = 0;
+        for (int i = 0; i <= n; i++) {
+            total += i;
+        }
+        return total;
     }
 
     
     
-    int getInputNumber() { // gee, may thi sis useful in another lab?
+    static int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
-        
-
-        // This method reads the number provided using keyboard
         int num = scan.nextInt();
 
         // Closing Scanner after the use
@@ -36,7 +40,7 @@ public class Main {
         return num;
     }
     
-    
+
     
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
@@ -46,4 +50,10 @@ public class Main {
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
+
+
+
 }
+
+
+
