ffaffbc6a3ce58b57f5f63e9f7547a6588b89ef8
tb0902 <tbruton417@gmail.com>
2024-03-01 08:20:38 -0500

    added scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
@@ -19,11 +20,13 @@ public class Main {
     int sumOfNumbers(int n) {
         return 0;
     }
+
     
     
     int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
+        
 
         // This method reads the number provided using keyboard
         int num = scan.nextInt();
